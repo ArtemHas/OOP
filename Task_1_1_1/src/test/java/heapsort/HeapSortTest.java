c0e8f5a32acadc785cb28822242295565046172e
Artem Shipilov <a.shipilov@g.nsu.ru>
2026-09-08 00:15:18 +0700

    changed style

--- Task_1_1_1/src/test/java/heapsort/HeapSortTest.java
+++ Task_1_1_1/src/test/java/heapsort/HeapSortTest.java
@@ -1,36 +1,40 @@
 package heapsort;
+
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class HeapSortTest {
     @Test
-    void testGeneral(){
-        int [] input = {4,10,3,5,1,2};
-        int [] expected = {1,2,3,4,5,10};
+    void testGeneral() {
+        int[] input = {4, 10, 3, 5, 1, 2};
+        int[] expected = {1, 2, 3, 4, 5, 10};
 
         HeapSort.heapSort(input);
         assertArrayEquals(expected, input);
     }
+
     @Test
-    void testAleradySorted(){
-        int [] input = {1,2,3,4,5,10};
-        int [] expected = {1,2,3,4,5,10};
+    void testAleradySorted() {
+        int[] input = {1, 2, 3, 4, 5, 10};
+        int[] expected = {1, 2, 3, 4, 5, 10};
         HeapSort.heapSort(input);
         assertArrayEquals(expected, input);
     }
 
     @Test
     void testReverseSorted() {
-        int[] input = {10,5,4,3,2,1};
-        int [] expected = {1,2,3,4,5,10};
+        int[] input = {10, 5, 4, 3, 2, 1};
+        int[] expected = {1, 2, 3, 4, 5, 10};
 
         HeapSort.heapSort(input);
         assertArrayEquals(expected, input);
     }
 
     @Test
-    void testWithDuplicates(){
-        int [] input = {4,10,3,5,1,2,5};
-        int [] expected = {1,2,3,4,5,5,10};
+    void testWithDuplicates() {
+        int[] input = {4, 10, 3, 5, 1, 2, 5};
+        int[] expected = {1, 2, 3, 4, 5, 5, 10};
 
         HeapSort.heapSort(input);
         assertArrayEquals(expected, input);
