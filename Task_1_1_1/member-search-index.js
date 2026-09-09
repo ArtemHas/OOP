a15bfa76f41d1105a6fd77667e25c479eecf1331
ArtemHas <ArtemHas@users.noreply.github.com>
2026-09-09 15:29:36 +0000

    Deploying to gh-pages from @ ArtemHas/OOP@2e97f70c912d3b794ccb5776ec8e1bc737642bf4 🚀

--- Task_1_1_1/member-search-index.js
+++ Task_1_1_1/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"heapsort","c":"HeapSort","l":"HeapSort()","u":"%3Cinit%3E()"}];updateSearchResults();
+memberSearchIndex = [{"p":"heapsort","c":"HeapSort","l":"HeapSort()","u":"%3Cinit%3E()"},{"p":"heapsort","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"heapsort","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
