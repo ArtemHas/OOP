5bb6ef11588f910f0625ad898bb6f4ebe4761e66
Artem Shipilov <a.shipilov@g.nsu.ru>
2026-09-09 22:30:29 +0700

    changed style

--- Task_1_1_1/src/main/java/heapsort/Main.java
+++ Task_1_1_1/src/main/java/heapsort/Main.java
@@ -1,12 +1,18 @@
 package heapsort;
+
 import java.util.Arrays;
 import java.util.Random;
-
+/*
+Main class
+ */
 public class Main {
-    public static void main(String [] args){
-        int [] arr = new int[10];
+    /*
+    Main method
+    */
+    public static void main(String[] args) {
+        int[] arr = new int[10];
         Random rand = new Random();
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = rand.nextInt(100);
         }
         System.out.println("Сгенерированный массив: " + Arrays.toString(arr));
